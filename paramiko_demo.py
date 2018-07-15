aad5ae5cb80b1a093f12099674d1f923d8dd23c5
chen <312197153@qq.com>
2018-07-15 18:23:17 +0800

    "添加paramiko简单测试"

--- paramiko_demo.py
+++ paramiko_demo.py
@@ -9,60 +9,52 @@ class SshClient(object):
         self.password = password
         self.port = port
         self.ssh = None
+        self.chan = None
 
     def connect(self):
         ssh_client = paramiko.SSHClient()
         ssh_client.set_missing_host_key_policy(paramiko.MissingHostKeyPolicy())
         ssh_client.connect(hostname=self.host, username=self.username, password=self.password, port=self.port)
+        trans = ssh_client.get_transport()
+        chan = trans.open_session()
+        chan.get_pty(width=200)
+        chan.invoke_shell()
+
+        self.chan = chan
         self.ssh = ssh_client
 
     # 每次执行都会打开一个新的channel
     def exec_cmd(self, cmd):
         stdin, stdout, stderr = self.ssh.exec_command(cmd)
 
-        return stdout.read()
+        # return stdout.read().decode()
+        # if self.chan.recv_exit_status():
+        ret = stdout.read()
+        # else:
+        #     ret = b"xxx"
+        # print(ret)
+        # print("\n")
+        # print(ret.decode())
+        return ret.decode()
+
+    def send_cmd(self, cmd):
+        self.chan.send(cmd)
+
+    def send_cmds(self, cmds):
+        for cmd in cmds:
+            self.chan.send(cmd)
+
+    def get_result_from_chan(self):
+        ret = self.chan.recv(1024)
+        return ret.decode()
 
     def close(self):
         self.ssh.close()
 
 
-class InteractiveSsh(object):
-    def __init__(self, host="10.10.57.1", username="chenguang", password="112992", port=22):
-        self.host = host
-        self.username = username
-        self.password = password
-        self.port = port
-        self.shell = None
-        self.transport = None
-
-    def connect(self):
-        trans = paramiko.Transport((self.host, self.port))
-        trans.connect(username=self.username, password=self.password)
-        self.transport = trans
-        chan = trans.open_channel()
-        chan.get_pty()
-        chan.invoke_shell()
-
-        self.shell = chan
-
-    def exec_cmd(self, cmd):
-        self.shell.send(cmd)
-        result = self.shell.recv(1024).decode()
-        return result
-
-    def exec_cmds(self, cmds):
-        result = list()
-        for cmd in cmds:
-            self.shell.send(cmd)
-            time.sleep(1)
-            result.append(self.shell.recv(1024).decode())
-
-        return result
-
-    def close(self):
-        if  self.shell:
-            self.shell.close()
-        if  self.transport:
-            self.transport.close()
-
-
+if __name__ == '__main__':
+    ssh = SshClient()
+    ssh.connect()
+    ret = ssh.exec_cmd("ls -a")
+    print(ret)
+    ssh.close()
--- unitest_demo.py
+++ unitest_demo.py
@@ -1,8 +1,38 @@
 import unittest
 from HTMLTestRunner import HTMLTestRunner
+from paramiko_demo import *
+from unittest import findTestCases, makeSuite, TestLoader
+import time
 
 
 class ParamikoTest(unittest.TestCase):
+    # def __init__(self):
+    #     self.ssh = SshClient()
+    #     super().__init__()
+
+    def setUp(self):
+        self.ssh = SshClient()
+        self.ssh.connect()
+
+    def tearDown(self):
+        self.ssh.close()
+
+    def test_paramiko_exec_cmd(self):
+        out = self.ssh.exec_cmd("touch chen/guang1")
+        print(not(out))
+        out1 = out.split("\n")
+        print(repr(out), out1)
+        self.assertSetEqual(set(["chen1", "chen2", "chen3"]), set(out1))
+
+    def test_paramiko_send_cmd(self):
+        self.ssh.get_result_from_chan()
+        time.sleep(1)
+        self.ssh.send_cmd("ls chen/\n")
+        time.sleep(2)
+        ret = self.ssh.get_result_from_chan()
+        print(repr(ret))
+        self.assertSetEqual(set(["chen1", "chen2", "chen3"]), set(ret))
+
     def test_1(self):
         self.assertEqual("a", "a", "相等")
 
@@ -11,8 +41,10 @@ class ParamikoTest(unittest.TestCase):
 
 
 if __name__ == "__main__":
-    test_suit = unittest.TestSuite()
-    test_suit.addTests([ParamikoTest("test_1"), ParamikoTest("test_2")])
+    # test_suit = unittest.TestSuite()
+    test_suit = TestLoader().loadTestsFromTestCase(ParamikoTest)
+    # test_suit.addTests([ParamikoTest("test_1"), ParamikoTest("test_2")])
+    suit = unittest.TestSuite([test_suit])
     with open("report.html", "wb") as f:
         runner = HTMLTestRunner(stream=f, title="test_report", description="html", verbosity=2)
-        runner.run(test_suit)
+        runner.run(suit)
